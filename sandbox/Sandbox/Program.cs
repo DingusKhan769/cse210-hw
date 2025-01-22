21c89d1581e112c4e8fca12b456af5bed2dc35cc
DingusKhan769 <afk2005@outlook.com>
2025-01-21 18:47:37 -0700

    Quick Classes lesson
    Working with classes sandbox

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,9 +1,33 @@
 using System;
+using System.Diagnostics;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        Moive favoriteMovie = new Moive();
+        favoriteMovie._title = "Star Wars";
+        favoriteMovie._year = 1977;
+        favoriteMovie._runtime = 150;
+        favoriteMovie._rating = "PG";
+
+        Moive otherMovie = new Moive();
+        otherMovie._title = "Avatar";
+        otherMovie._year = 2009;
+        otherMovie._runtime = 162;
+        otherMovie._rating = "PG-13";
+
+        Console.WriteLine(favoriteMovie._title);
+
+        favoriteMovie.Display();
+
+        DisplayMovie(favoriteMovie);
+        DisplayMovie(otherMovie);
+        
+    }
+
+    static void DisplayMovie(Moive aMovie)
+    {
+        Console.WriteLine($"{aMovie._title} - {aMovie._year}");
     }
 }
