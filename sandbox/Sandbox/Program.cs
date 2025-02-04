3e8bd1c283cba45888023760993859dd9d10f0eb
DingusKhan769 <afk2005@outlook.com>
2025-02-04 16:15:29 -0700

    SadnBox stuff

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,29 +5,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Moive favoriteMovie = new Moive();
-        favoriteMovie._title = "Star Wars";
-        favoriteMovie._year = 1977;
-        favoriteMovie._runtime = 150;
-        favoriteMovie._rating = "PG";
-
-        Moive otherMovie = new Moive();
-        otherMovie._title = "Avatar";
-        otherMovie._year = 2009;
-        otherMovie._runtime = 162;
-        otherMovie._rating = "PG-13";
-
-        Console.WriteLine(favoriteMovie._title);
-
-        favoriteMovie.Display();
-
-        DisplayMovie(favoriteMovie);
-        DisplayMovie(otherMovie);
+        Person p1 = new Person();
+        p1.SetName("Jerry");
+        p1.SetAge(23);
         
+        p1.Display();
+
+        string theName = p1.GetName();
+        Console.WriteLine($"The name is {theName}");
     }
 
-    static void DisplayMovie(Moive aMovie)
-    {
-        Console.WriteLine($"{aMovie._title} - {aMovie._year}");
-    }
 }
